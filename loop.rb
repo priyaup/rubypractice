218b32f1d97e5cd0990d84bb7ee3bb6cba0ac6da
Priya Upadhyay <upriya232@gmail.com>
2021-11-10 18:22:33 +0530

    [10112021]practicing loop

--- loop.rb
+++ loop.rb
@@ -1,4 +1,42 @@
-num = 1
-for num in(0..5)
-	puts num
-end
+
+#for Loop
+
+# num = 1
+# for num in(0..5)
+# 	puts num
+# end
+
+
+
+#while loop
+
+# i=0
+# while(i<=5)
+# 	puts i
+# 	i = i+1
+# end
+
+
+
+#until loop
+
+# i=0
+# until (i>=5)
+# 	puts i
+# 	i = i+1	
+# end
+
+
+#practice for Loop
+
+# i = 0
+# no = 30
+#  for i in (0..no)
+#  	if (i%2 == 0)
+#  		puts i;
+#  	end
+#  end
+    
+
+
+

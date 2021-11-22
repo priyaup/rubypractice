ec18d79bf33662e42648c77bb63fc1eb27d0862c
Priya Upadhyay <upriya232@gmail.com>
2021-11-22 18:55:54 +0530

    [22112021] practicing array

--- array_practice.rb
+++ array_practice.rb
@@ -1,10 +1,43 @@
- a = []
+ # a = []
 
- a << 4 << 3 << 10 << 9
- puts a
+ # a << 4 << 3 << 10 << 9
+ # puts a
 
 # a = Array.new(5 , "priya")
 #  puts a
 
 
 
+#Array Practice
+
+# arr = [73 , 98, 86, 61, 96]
+# i = 0;
+# max = 0;
+
+# while ( i < arr.size)
+#     if (arr[i] > max)
+
+#          max = arr[i]
+        
+#     end
+#     i = i + 1
+    
+# end
+# puts max;
+
+arr = [73 , 98, 86, 61, 96]
+ i = 0;
+ min = 0;
+
+ while ( i < arr.size)
+     if (arr[i] <  min)
+
+          min = arr[i]
+        
+     end
+     i = i + 1
+    
+ end
+ puts min;
+
+    

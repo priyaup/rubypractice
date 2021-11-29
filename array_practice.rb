be7a389ef453bd843471edf3556b1e8922d63e7b
Priya Upadhyay <upriya232@gmail.com>
2021-11-29 18:49:15 +0530

    [29112021]Practicing array

--- array_practice.rb
+++ array_practice.rb
@@ -25,21 +25,56 @@
 # end
 # puts max;
 
-arr = [73 , 98, 86, 61, 96]
- i = 0;
- min = 0;
+ # arr = [73 , 98, 86, 61, 96]
 
- while ( i < arr.size)
-     if (arr[i] <  min)
+ # i = 0;
 
-          min = arr[i]
-        
-     end
-     i = i + 1
-    
- end
- puts min;
+ # min = arr[0] ;
+
+ # a = 0;
+
+ # while ( i < arr.size)
+ #    if (min > arr[i])
+ #      a = arr[i]
+ #      arr[i] = min
+ #      min = a
+ #    end
+ #     i += 1
+ #  end
+ # puts min;
 
  #practicing array 
 
     
+arr = [1,2,3,4,5,6,-10 ,-90 , -8 ,30, 60]
+
+positive_sum = 0
+
+negative_sum = 0
+
+i = 0;
+
+while ( i < arr.size)
+
+  if ( arr[i] > 0)
+   
+   positive_sum += arr[i]
+
+  elsif ( arr[i] < 0)
+
+   negative_sum += arr[i]
+  
+  end
+
+  i += 1
+
+end
+
+puts "Sum of positive number is:#{positive_sum}"
+
+puts "Sum of negative number is:#{negative_sum}"
+
+
+
+
+

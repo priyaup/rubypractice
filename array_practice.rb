d053de958daf9e11bd729ca30940d21d335de23d
Priya Upadhyay <upriya232@gmail.com>
2021-12-01 19:05:13 +0530

    [11221]Practicing array

--- array_practice.rb
+++ array_practice.rb
@@ -43,36 +43,73 @@
  #  end
  # puts min;
 
+
+
  #practicing array 
 
+
+ # The sum of Positive and negative number 
+
     
-arr = [1,2,3,4,5,6,-10 ,-90 , -8 ,30, 60]
+# arr = [1,2,3,4,5,6,-10 ,-90 , -8 ,30, 60]
 
-positive_sum = 0
+# positive_sum = 0
 
-negative_sum = 0
+# negative_sum = 0
+
+# i = 0;
+
+# while ( i < arr.size)
+
+#   if ( arr[i] > 0)
+   
+#    positive_sum += arr[i]
+
+#   elsif ( arr[i] < 0)
+
+#    negative_sum += arr[i]
+  
+#   end
+
+#   i += 1
+
+# end
+
+# puts "Sum of positive number is:#{positive_sum}"
+
+# puts "Sum of negative number is:#{negative_sum}"
+
+
+arr = [12015, 12932, 18146, 18265]
 
 i = 0;
 
+b = 0;
+
+j =0;
+
 while ( i < arr.size)
 
-  if ( arr[i] > 0)
-   
-   positive_sum += arr[i]
+    b = arr[i]
+    while (j < 5)
 
-  elsif ( arr[i] < 0)
+       r = b % 10
 
-   negative_sum += arr[i]
+       b = b / 10
+
+      j += 1
+
+    end
+    
+
+    i += 1
+    puts b
   
-  end
-
-  i += 1
-
 end
+  
+  
 
-puts "Sum of positive number is:#{positive_sum}"
 
-puts "Sum of negative number is:#{negative_sum}"
 
 
 

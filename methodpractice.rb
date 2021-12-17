57ab44149b7d82edff3ea306819e541b5ac85859
Priya Upadhyay <upriya232@gmail.com>
2021-12-17 19:21:36 +0530

    [171221]Practice Array methods

---
+++ methodpractice.rb
@@ -0,0 +1,41 @@
+#methods of ruby
+
+arr = [1, 2, 3, 4, 5 ]
+
+#puts arr.take(3)
+
+#puts arr.drop(3)
+
+#arr.push(7) { Insert the element in last of array}
+
+#arr.pop { For removing the last element of array}
+
+#arr.unshift { Inserting the element in begining of array }
+
+#arr.product { It convert the each element of array into the array}
+
+#puts arr[2, 3]
+
+#puts arr[0..-4]
+
+#puts arr.min { it select the min element for an array}
+
+#puts arr.max { it select the max value for an array} 
+
+#puts arr.flatten { merge the two array = [1,2,3 ,[4,5]] = 1,2,3,4,5}
+
+#puts arr.at(3) {Another way to access a particular array element is by using the at method}
+
+#puts arr. fetch(5, "sorry") { To raise an error for indices outside of the array bounds or else to provide a default value when that happens, you can use fetch.}
+
+#puts arr.first { gives 1st element}
+
+#puts arr.last { gives last element}
+
+#puts arr.length { give the lenght of array and size, count also give same result}
+
+#puts arr.insert(4, "Priya") { you can a new element of an array at any position}
+
+#puts arr.delete_at(3) {To delete an element at a particular index}
+
+puts arr.delete(3)

e75a573116d10716ee7982e1dd323eabaa0c20ca
Priya Upadhyay <upriya232@gmail.com>
2021-12-23 18:51:27 +0530

    [231221]practicing hash

---
+++ hash.rb
@@ -0,0 +1,23 @@
+ # person_info = {
+    
+ #  "first_name" => 'Priya',
+ #  "Last_name" => 'Upadhyay',
+ #  "email" => 'xyz@gmail.com',
+ #  "contact" => '12345'
+
+ # }
+
+ #  puts person_info['first_name']
+
+
+ person_info = {
+    
+    name: 'priya',
+
+    branch: 'csit',
+
+    email: 'xyz@gmail.com',
+
+ }
+
+ puts person_info[:branch]

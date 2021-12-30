7042ff604136b58c06649212f68ab0b3f3ce626a
Priya Upadhyay <upriya232@gmail.com>
2021-12-30 19:06:20 +0530

    [301221]practicing hash

--- hash.rb
+++ hash.rb
@@ -23,7 +23,36 @@
  # puts person_info[:branch]
 
 
- time1 = Time.new
-# puts "Current Time : " + time1.inspect
+# time1 = Time.strftime
+#  puts "Current Time : " + time1.inspect
+# puts time1.sec
+# puts time1.min
+# puts time1.wday
+# puts time1.yday
+# puts time1.year
+# puts time1.zone
 
-puts time1.year
+#Range
+
+# alphabet = ('a'..'z').to_a
+
+# puts alphabet
+
+# priya = ('priyaa'..'priyaz').to_a
+
+# puts priya
+
+
+# range = (1..10).to_a
+
+# if ((1..10)/2 === 0)
+# puts range
+# end
+
+def sum (a , b)
+	puts a + b
+	end
+
+	sum(20 , 5)
+
+
